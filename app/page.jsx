cdbff9edfd9ccd6dade5287b74c6d85877ba371f
Garrett Hoffman <son0hoff@gmail.com>
2023-06-15 23:29:58 -0700

    basic home page setting

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptly is an open-source tool to collaborate and share AI powered
+        prompts for a simpler tomorrow
+      </p>
+    </section>
+  );
 };
 
 export default Home;

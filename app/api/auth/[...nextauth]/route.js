f0f062a7f76987fd8d560c4aa40a071bd04245b3
Garrett Hoffman <son0hoff@gmail.com>
2023-06-30 18:17:01 -0700

    begin serverless api routes for backend

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -12,6 +14,13 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       //serverless
+      await connectToDB();
+
+      // check if a user already exists
+
+      // if not, create a new user
+
+      return true;
     } catch (error) {}
   },
 });

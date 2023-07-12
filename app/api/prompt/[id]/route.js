74b4edaad3b5eae80780cd81266250dabdc88903
Garrett Hoffman <son0hoff@gmail.com>
2023-07-12 16:28:54 -0700

    Delete route for prompts. Typically would run a check on user to lock the route instead of locking through front-end which is the current siutation, but this is just an example

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -38,3 +38,14 @@ export const PATCH = async (request, { params }) => {
 };
 
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete prompt", { status: 500 });
+  }
+};
